74ef8d091ecc097ef75332d76c47b685c7ac4132
jasonkim72888 <jason.kim7288@gmail.com>
2024-01-14 20:31:31 +1000

    change icon

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Main = () => {
   return (
     <div className="container">
-      <h3 className="mt-5 pt-5 text-center" style={{ height: '600px' }}>Muse! Quotes</h3>
+      <h3 className="mt-5 pt-5 text-center" style={{ height: '600px' }}>Muse</h3>
     </div>
   )
 }
