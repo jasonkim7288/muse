a35791ffb0986f9401945e2edf9a70ed1f114e8a
jasonkim72888 <jason.kim7288@gmail.com>
2024-01-14 20:50:36 +1000

    contents changed

--- src/components/Privacy.js
+++ src/components/Privacy.js
@@ -16,12 +16,6 @@ const Privacy = () => {
     <div style={privacyStyles}>
       <h3>{t('Privacy Policy')}</h3>
       <p>{t('Privacy Policy main description')}</p>
-      <h5>{t('Analytics')}</h5>
-      <p>{t('Analytics description')}</p>
-      <h5>{t('Crash reports')}</h5>
-      <p>{t('Crash reports description')}</p>
-      {/* <h5>{t('Advertising')}</h5>
-      <p>{t('Advertising description')}</p> */}
       <h5>{t('Changes to this policy')}</h5>
       <p>{t('Changes to this policy description')}</p>
       <p className="mt-5">{t('Last modified with the date')}</p>
