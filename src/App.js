560e5f64e8151dd820aa843d2bd55cb204ce5101
Jason Kim <jason.kim7288@gmail.com>
2024-05-27 20:24:56 +1000

    change app-ads location

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Main from './components/Main';
 import Privacy from './components/Privacy';
-import AppAds from './components/AppAds';
+// import AppAds from './components/AppAds';
 
 const App = () => {
   return (
@@ -11,7 +11,7 @@ const App = () => {
         <Switch>
           <Route exact path="/" component={Main} />
           <Route path="/privacy" component={Privacy} />
-          <Route path="/app-ads.txt" component={AppAds} />
+          {/* <Route path="/app-ads.txt" component={AppAds} /> */}
         </Switch>
       </BrowserRouter>
     </>
