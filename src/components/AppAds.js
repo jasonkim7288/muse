da882aaad74e23e0d7d521eb85d2cf70ea5ee1e5
Jason Kim <jason.kim7288@gmail.com>
2024-12-19 20:53:24 +1000

    change app-ads

--- src/components/AppAds.js
+++ src/components/AppAds.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const AppAds = () => {
   return (
-    <p>google.com, pub-4953617168750655, DIRECT, f08c47fec0942fa0</p>
+    <p>google.com, pub-2033504803339109, DIRECT, f08c47fec0942fa0</p>
   );
 };
 
